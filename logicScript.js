2e3ac28a377a01cd08832000efba2af64ab50b97
Mac <sophanpa@gmail.com>
2022-07-30 17:49:04 +0200

    Edit playable now logicScritp.js

--- logicScript.js
+++ logicScript.js
@@ -4,7 +4,7 @@
 function getComputerChoice(){
     let num;
     num = Math.floor(Math.random() * 3);
-    let choice;
+    
     switch(num) {
         case 0: return"rock";
         break;
@@ -16,7 +16,29 @@ function getComputerChoice(){
         break;
     }
 
-    
+   
 }
 
-console.log(getComputerChoice())
+
+
+
+function playRound(player,com){
+
+ console.log(com);
+    if(player == "rock" && com == "scissor" || player == "paper" && com == "rock" || player == "scissor" && com == "paper"){
+        return "player won!"
+    } 
+    else if(player == com){
+        return "Tie!"
+    }
+    else {
+        return "com won"
+    }
+
+   
+}
+
+console.log(playRound(prompt(),getComputerChoice()));
+
+
+
