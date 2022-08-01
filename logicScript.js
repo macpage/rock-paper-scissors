031eb9f648aecf9d55fbf8393c59942ab77f331a
Mac <sophanpa@gmail.com>
2022-08-01 16:13:24 +0200

    Add base logic, value changes

--- logicScript.js
+++ logicScript.js
@@ -1,19 +1,19 @@
 
 let playerScore = 0;
 let comScore = 0;
-
+let comChoice;
 function getComputerChoice(){
     let num;
     num = Math.floor(Math.random() * 3);
     
     switch(num) {
-        case 0: return"rock";
+        case 0: return comChoice = "Rock";
         break;
 
-        case 1: return "paper";
+        case 1: return comChoice =  "Paper";
         break;
 
-        case 2: return  "scissor";
+        case 2: return comChoice =  "Scissors";
         break;
     }
 
@@ -26,7 +26,7 @@ function getComputerChoice(){
 function playRound(player,com){
 
  console.log(com);
-    if(player == "rock" && com == "scissor" || player == "paper" && com == "rock" || player == "scissor" && com == "paper"){
+    if(player == "rock" && com == "scissors" || player == "paper" && com == "rock" || player == "scissors" && com == "paper"){
         ++playerScore;
         return "player won!"
       
@@ -39,7 +39,7 @@ function playRound(player,com){
         return "com won"
     }
 
-   
+ 
 }
 
 
@@ -47,10 +47,40 @@ function playRound(player,com){
 
 function game(){
 
-
+  const pScore = document.querySelector("#player-score");
+  const cScore = document.querySelector("#com-score");
+   pScore.innerHTML = playerScore;
+   cScore.innerHTML = comScore;;
+   cChoice.innerHTML = '"'+comChoice+'"';
 }
 
-console.log(game());
+let pChoice = document.querySelector(".pChoice");
+let cChoice = document.querySelector(".cChoice");
+
+const btns = document.querySelector(".btns");
+btns.children[0].addEventListener("click", () => {
+    
+    playRound(btns.children[0].innerHTML.toLowerCase(),getComputerChoice().toLowerCase());
+    game();
+    pChoice.innerHTML = '"Rock!"';
+});
+btns.children[1].addEventListener("click", () => {
+    
+    playRound(btns.children[1].innerHTML.toLowerCase(),getComputerChoice().toLowerCase());
+    game();
+    pChoice.innerHTML = '"Paper!"';
+});
+btns.children[2].addEventListener("click", () => {
+    
+    playRound(btns.children[2].innerHTML.toLowerCase(),getComputerChoice().toLowerCase());
+    game();
+    pChoice.innerHTML = '"Scissors!"';
+});
+
+
+
+
+
 
 
 
